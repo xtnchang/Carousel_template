354606e1f25a6289fd51c4a33015356d79a71b2a
Christine Chang <xtnchang@gmail.com>
2017-08-18 16:05:24 -0700

    First commit feat: Implement collection view

---
+++ Carousel template/CarouselViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CarouselViewController.swift
+//  Carousel template
+//
+//  Created by Christine Chang on 8/18/17.
+//  Copyright © 2017 Christine Chang. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var carouselCollectionView: UICollectionView!
+    
+    let columns: CGFloat = 1.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        carouselCollectionView.dataSource = self
+        carouselCollectionView.delegate = self
+    }
+
+    // MARK: - CarouselViewController: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IndividualViewCell", for: indexPath) as! IndividualViewCell
+        
+        return cell
+    }
+}
+
+// MARK: - CarouselViewController: UICollectionViewDelegateFlowLayout
+
+extension CarouselViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = Int(collectionView.frame.width / columns)
+        let height = Int(collectionView.frame.height)
+        return CGSize(width: width, height: height)
+    }
+}
+
---
+++ Carousel template/IndividualViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IndividualViewCell.swift
+//  Carousel template
+//
+//  Created by Christine Chang on 8/18/17.
+//  Copyright © 2017 Christine Chang. All rights reserved.
+//
+
+import UIKit
+
+class IndividualViewCell: UICollectionViewCell {
+    
+}
